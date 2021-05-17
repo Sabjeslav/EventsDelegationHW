7b188553e85fdd5e4673d02229bc9d574e71d9ff
s4tenko <s4tenko@gmail.com>
2021-05-17 06:37:44 +0300

    Fixed td click bug

--- task2.js
+++ task2.js
@@ -20,19 +20,20 @@ table.addEventListener("click", ({ target }) => {
     deleteHandler(target);
     return;
   }
-  if (target.tagName === "TD") {
+  if (target.tagName === "SPAN") {
     const content = target.innerText;
     target.innerText = "";
-
     const input = createInput(content);
     target.append(input);
+  }
+});
 
-    input.addEventListener("keyup", (e) => {
-      if (e.code === "Enter") {
-        target.innerText = input.value;
-        input.remove();
-      }
-    });
+table.addEventListener("keyup", ({ target, code }) => {
+  if (target.tagName === "INPUT") {
+    if (code === "Enter") {
+      target.parentNode.textContent = target.value;
+      target.remove();
+    }
   }
 });
 
@@ -40,21 +41,27 @@ table.addEventListener("click", ({ target }) => {
 
 const addForm = document.querySelector(".addForm");
 
-function createTD(value) {
-  const td = document.createElement("td");
-  td.textContent = value;
-  return td;
+function createSpan(value) {
+  const span = document.createElement("span");
+  span.textContent = value;
+  return span;
+}
+
+function createTD() {
+  return document.createElement("td");
 }
 
 addForm.addEventListener("click", ({ target }) => {
   if (target.className === "add-btn") {
     const i1 = document.querySelector(".input-fname");
     const i2 = document.querySelector(".input-sname");
-    const fname = createTD(i1.value);
-    const sname = createTD(i2.value);
+    const fname = createTD();
+    fname.append(createSpan(i1.value));
+    const sname = createTD();
+    sname.append(createSpan(i2.value));
     const deleteTD = createTD();
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('delete-btn');
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
     deleteBtn.textContent = "Delete";
     deleteTD.append(deleteBtn);
     const tr = document.createElement("tr");
